0424f7227cfd9ccadd7008768961987920ef3551
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-04-16 19:16:18 +0500

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
+class Queue {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+
+  getUnderlyingList() {
+   return this.head;
+  }
+
+  enqueue(val){
+    const node = new ListNode(val);
+    if(!this.head){
+      this.head = node;
+      this.tail = node;
+    }else{
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+  dequeue() {
+    if(!this.head){
+      return undefined;
+    }
+    const val = this.head.value;
+    this.head = this.head.next;
+    if(this.head === null){
+      this.tail = null;
+    }
+    return val;
+  }
+}
 module.exports = {
   Queue
 };
